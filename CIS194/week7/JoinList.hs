69b5f03058dfb401da7fd94a58c012156f9e0bda
Nero Zuo <502350123@qq.com>
2017-01-23 00:41:15 +0800

    2

--- CIS194/week7/JoinList.hs
+++ CIS194/week7/JoinList.hs
@@ -2,6 +2,7 @@ module JoinList where
 
 import Data.Monoid
 
+import Sized
 import Buffer
 import Editor
 
@@ -10,6 +11,7 @@ data JoinList m a = Empty
                    | Append m (JoinList m a) (JoinList m a)
    deriving (Eq, Show)
 
+-- Exercise 1
 tag :: Monoid m => JoinList m a -> m
 tag (Single m _) = m
 tag (Append m _ _) = m
@@ -18,3 +20,27 @@ tag Empty = mempty
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
 (+++) l r = Append (tag l <> tag r) l r
 
+-- Exercise 2
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ index (Single _ a) 
+    | index == 0 = Just a
+    | otherwise = Nothing
+indexJ index (Append m l r) 
+    | index < 0 || index >= totalSize = Nothing
+    | index < lSize       = indexJ index l
+    | otherwise           = indexJ (index - lSize) r
+        where totalSize = getSize $ size m
+              lSize     = getSize $ size $ tag l
+indexJ _ _ = Nothing
+
+
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ 0 jl = jl
+dropJ n jl@(Append m l r)
+    | n >= totalSize = Empty
+    | n >= lSize     = dropJ (n - lSize) r
+    | n >= 0         = dropJ n l
+        where totalSize = getSize $ size m
+              lSize     = getSize $ size $ tag l
+dropJ n _  
+    | n>0 = Empty
