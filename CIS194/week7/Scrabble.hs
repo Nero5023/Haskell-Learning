24f1103974201246a62bb58707f8f7fb8f443d4c
Nero Zuo <502350123@qq.com>
2017-01-23 12:34:45 +0800

    exercise 3

---
+++ CIS194/week7/Scrabble.hs
@@ -0,0 +1,30 @@
+module Scrabble where
+
+import Data.Monoid
+import Data.Char
+import JoinList
+
+newtype Score = Score Int
+    deriving (Eq, Read, Show, Ord)
+
+instance Monoid Score where
+    mempty  = Score 0
+    mappend (Score l) (Score r) = Score (l+r)
+
+score :: Char -> Score
+score ch
+    | chLow `elem` "aeilnorstu" = Score 1
+    | chLow `elem` "dg"         = Score 2
+    | chLow `elem` "bcmp"       = Score 3
+    | chLow `elem` "fhvwy"      = Score 4
+    | chLow `elem` "k"          = Score 5
+    | chLow `elem` "jx"         = Score 8
+    | chLow `elem` "qz"         = Score 10
+    | otherwise              = Score 0
+        where chLow = toLower ch
+
+scoreString :: String -> Score
+scoreString = foldl (\acc x -> acc <> score x) $ Score 0 
+
+scoreLine :: String -> JoinList Score String
+scoreLine str = Single (scoreString str) str
