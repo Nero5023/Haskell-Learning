7f8e641fe90e8b81820f841daac005ae6a80ebe4
Nero Zuo <502350123@qq.com>
2017-01-31 14:14:54 +0800

    exercise1-2

--- CIS194/week8/Party.hs
+++ CIS194/week8/Party.hs
@@ -2,6 +2,14 @@ module Party where
 import Employee
 
 -- Exercise 1
+-- 1
 glCons :: Employee -> GuestList -> GuestList
 glCons emp@(Emp{ empFun = fun}) (GL guests funs) 
     = GL (emp:guests) (fun + funs)
+
+-- 2
+instance Monoid GuestList where
+    mempty = GL [] 0
+    mappend (GL list0 fun0) (GL list1 fun1)
+        = GL (list0 ++ list1) $ fun0 + fun1
+    
