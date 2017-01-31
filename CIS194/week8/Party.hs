1295bd6955ca958f70118ade82777a68c6df5898
Nero Zuo <502350123@qq.com>
2017-01-31 14:17:57 +0800

    exercise1-3

--- CIS194/week8/Party.hs
+++ CIS194/week8/Party.hs
@@ -12,4 +12,9 @@ instance Monoid GuestList where
     mempty = GL [] 0
     mappend (GL list0 fun0) (GL list1 fun1)
         = GL (list0 ++ list1) $ fun0 + fun1
+
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun gl0@(GL _ fun0) gl1@(GL _ fun1)
+    | fun0 >= fun1 = gl0
+    | otherwise    = gl1
     
