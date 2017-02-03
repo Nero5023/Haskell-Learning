02066dfdab33c7bf9e9a809c1e64eb3d048e3b2b
Nero Zuo <502350123@qq.com>
2017-02-04 01:47:14 +0800

    finish week11

--- CIS194/week11/SExpr.hs
+++ CIS194/week11/SExpr.hs
@@ -48,3 +48,13 @@ data Atom = N Integer | I Ident
 data SExpr = A Atom
            | Comb [SExpr]
   deriving Show
+
+atom :: Parser Atom
+atom = N <$> posInt <|> I <$> ident
+
+parseSExpr :: Parser SExpr
+parseSExpr = spaces *> 
+            ((A <$> atom)
+             <|>
+             char '(' *> (Comb <$> oneOrMore parseSExpr) <* char ')') 
+            <* spaces
