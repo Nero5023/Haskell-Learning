df5a57718464a0d7924ed2023d615a67768905f8
Nero Zuo <502350123@qq.com>
2017-02-07 16:14:13 +0800

    finish exercise 2

--- CIS194/week12/Risk.hs
+++ CIS194/week12/Risk.hs
@@ -3,6 +3,9 @@
 module Risk where
 
 import Control.Monad.Random
+import Control.Monad
+import System.Random
+import Data.List
 
 ------------------------------------------------------------
 -- Die values
@@ -20,9 +23,45 @@ instance Random DieValue where
 die :: Rand StdGen DieValue
 die = getRandom
 
+dieN :: Int -> Rand StdGen [DieValue]
+dieN n = replicateM n die
+
 ------------------------------------------------------------
 -- Risk
 
 type Army = Int
 
-data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+data Battlefield = Battlefield { attackers :: Army, defenders :: Army } 
+    deriving Show
+
+
+-- Exercise 2
+
+battle :: Battlefield -> Rand StdGen Battlefield
+battle bf = dieN (att+def) >>= \dvs -> 
+        return $ lostArmy bf $ battleOutcom (take att dvs) (drop att dvs)
+    where (att, def) = getADArm bf
+
+-- get the attacks and defends for the Battlefield
+getADArm :: Battlefield -> (Army, Army)
+getADArm (Battlefield att def) = (att', def')
+    where att' = if att > 5 then 3 else att-1
+          def' = if def > 2 then 2 else def
+
+-- Return the loss of armys for attackers and defenders
+-- args :: the first is the attackers dice values
+--         the second is the defenders dice values
+battleOutcom :: [DieValue] -> [DieValue] -> (Army, Army)
+battleOutcom atts defs = (elemNum False cmps, elemNum True cmps)
+    where atts' = sortBy (flip compare) atts
+          defs' = sortBy (flip compare) defs
+          cmps  = zipWith (>) atts' defs'
+
+elemNum :: (Eq a) => a -> [a] -> Int
+elemNum element
+    = foldl (\ acc x -> if x == element then acc + 1 else acc) 0 
+
+
+lostArmy :: Battlefield -> (Army, Army) -> Battlefield
+lostArmy (Battlefield att def) (lostAtt, lostDef) 
+    = Battlefield (att - lostAtt) (def - lostDef)
