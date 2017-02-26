def561aad7f8d355e6a08d9a4f18d091a836b5b4
Nero Zuo <502350123@qq.com>
2017-02-26 20:28:01 +0800

    no message

--- CS240h/Lecture2/test.hs
+++ CS240h/Lecture2/test.hs
@@ -10,3 +10,31 @@ public class TestAdder {
         assert(adder.add(1234, 988) == 2222);
     }
 }
+
+
+public class TestAdder {
+    public void testSum() {
+        Adder adder = new AdderImpl();
+        assert(adder.add(1, 1) == 2);
+        assert(adder.add(1, 2) == 3);
+        assert(adder.add(2, 2) == 4);
+        assert(adder.add(0, 0) == 0);
+        assert(adder.add(-1, -2) == -3);
+        assert(adder.add(-1, 1) == 0);
+        assert(adder.add(1234, 988) == 2222);
+    }
+}
+
+
+encodeChar :: Char -> [Word16]
+encodeChar x
+  | w < 0x10000 = [fromIntegral w]
+  | otherwise   = [fromIntegral a, fromIntegral b]
+  where w = ord x
+        a = ((w - 0x10000) `shiftR` 10) + 0xD800
+        b = (w .&. 0x3FF) + 0xDC00
+
+encodeChar' :: Char -> [Word16]
+encodeChar' x
+  | w < 0x10000 = [fromIntegral w]
+  where w = ord x
