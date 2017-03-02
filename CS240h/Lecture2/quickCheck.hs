fa86c73e7e47f4719de0f897e0c14f21dd0a3650
Nero Zuo <502350123@qq.com>
2017-03-02 23:11:09 +0800

    no message

--- CS240h/Lecture2/quickCheck.hs
+++ CS240h/Lecture2/quickCheck.hs
@@ -77,3 +77,14 @@ instance (Arbitrary a) => Arbitrary (Point a) where
     arbitrary = liftM2 Point arbitrary arbitrary
     -- TODO: provide a body for shrink
     shrink = undefined
+
+instance (Arbitrary a) => Arbitrary (Tree a) where
+    arbitrary = sized tree
+
+tree :: (Arbitrary a) => Int -> Gen (Tree a)
+tree 0 = liftM Leaf arbitrary
+tree n = oneof [
+           liftM  Leaf arbitrary
+         , liftM2 Node subtree subtree
+         ]
+  where subtree = tree (n `div` 2)
