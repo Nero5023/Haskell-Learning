9e3781c9b5cc34cbdcfbddbf9c330b82d96cf942
Nero Zuo <502350123@qq.com>
2017-03-04 23:25:56 +0800

    no message

--- CS240h/Lecture2/quickCheck.hs
+++ CS240h/Lecture2/quickCheck.hs
@@ -30,37 +30,9 @@ prop_encodeOne3 (Big c) = length (encodeChar c) == 1
 shrinkChar :: Char -> [Char]
 shrinkChar c = map chr $ shrink $ ord c
 
-data Tree a = Node (Tree a) (Tree a)
-            | Leaf a
-              deriving (Show)
-
-
-instance (Arbitrary a) => Arbitrary (Tree a) where
-    arbitrary = oneof [
-                  liftM  Leaf arbitrary
-                , liftM2 Node arbitrary arbitrary
-                ]
-
-
-tree :: (Arbitrary a) => Int -> Gen (Tree a)
-tree 0 = liftM Leaf arbitrary
-tree n = oneof [
-           liftM  Leaf arbitrary
-         , liftM2 Node subtree subtree
-         ]
-  where subtree = tree (n `div` 2)
-
-
-
-
-
-
-
-
-prop_encodeOne2 = do
-  c <- choose ('\0', '\xFFFF')
+prop_encodeOne2 = do 
+  c <- choose('\0', '\xFFFF')
   return $ length (encodeChar c) == 1
-
 
 prop_encodeOne4 (Big c) =
   (c < '\x10000') ==> length (encodeChar c) == 1
@@ -69,40 +41,80 @@ prop_encodeOne5 = do
   Big c <- arbitrary `suchThat` (< Big '\x10000')
   return $ length (encodeChar c) == 1
 
-class Functor f  where
-    fmap :: (a -> b) -> f a -> f b
+
+-- data Tree a = Node (Tree a) (Tree a)
+--             | Leaf a
+--               deriving (Show)
 
 
-data Point a = Point a a
-
-instance (Arbitrary a) => Arbitrary (Point a) where
-    arbitrary = do
-      x <- arbitrary
-      y <- arbitrary
-      return (Point x y)
-After:
-
-import Control.Monad (liftM2)
-
-instance (Arbitrary a) => Arbitrary (Point a) where
-    arbitrary = liftM2 Point arbitrary arbitrary
+-- instance (Arbitrary a) => Arbitrary (Tree a) where
+--     arbitrary = oneof [
+--                   liftM  Leaf arbitrary
+--                 , liftM2 Node arbitrary arbitrary
+--                 ]
 
 
-data Point a = Point a a
-               deriving (Eq, Show)
+-- tree :: (Arbitrary a) => Int -> Gen (Tree a)
+-- tree 0 = liftM Leaf arbitrary
+-- tree n = oneof [
+--            liftM  Leaf arbitrary
+--          , liftM2 Node subtree subtree
+--          ]
+--   where subtree = tree (n `div` 2)
 
-instance (Arbitrary a) => Arbitrary (Point a) where
-    arbitrary = liftM2 Point arbitrary arbitrary
-    -- TODO: provide a body for shrink
-    shrink = undefined
 
-instance (Arbitrary a) => Arbitrary (Tree a) where
-    arbitrary = sized tree
 
-tree :: (Arbitrary a) => Int -> Gen (Tree a)
-tree 0 = liftM Leaf arbitrary
-tree n = oneof [
-           liftM  Leaf arbitrary
-         , liftM2 Node subtree subtree
-         ]
-  where subtree = tree (n `div` 2)
+
+
+
+
+
+-- prop_encodeOne2 = do
+--   c <- choose ('\0', '\xFFFF')
+--   return $ length (encodeChar c) == 1
+
+
+-- prop_encodeOne4 (Big c) =
+--   (c < '\x10000') ==> length (encodeChar c) == 1
+
+-- prop_encodeOne5 = do
+--   Big c <- arbitrary `suchThat` (< Big '\x10000')
+--   return $ length (encodeChar c) == 1
+
+-- class Functor f  where
+--     fmap :: (a -> b) -> f a -> f b
+
+
+-- data Point a = Point a a
+
+-- instance (Arbitrary a) => Arbitrary (Point a) where
+--     arbitrary = do
+--       x <- arbitrary
+--       y <- arbitrary
+--       return (Point x y)
+-- After:
+
+-- import Control.Monad (liftM2)
+
+-- instance (Arbitrary a) => Arbitrary (Point a) where
+--     arbitrary = liftM2 Point arbitrary arbitrary
+
+
+-- data Point a = Point a a
+--                deriving (Eq, Show)
+
+-- instance (Arbitrary a) => Arbitrary (Point a) where
+--     arbitrary = liftM2 Point arbitrary arbitrary
+--     -- TODO: provide a body for shrink
+--     shrink = undefined
+
+-- instance (Arbitrary a) => Arbitrary (Tree a) where
+--     arbitrary = sized tree
+
+-- tree :: (Arbitrary a) => Int -> Gen (Tree a)
+-- tree 0 = liftM Leaf arbitrary
+-- tree n = oneof [
+--            liftM  Leaf arbitrary
+--          , liftM2 Node subtree subtree
+--          ]
+--   where subtree = tree (n `div` 2)
