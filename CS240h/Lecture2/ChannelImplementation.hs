cf0825dfd178849b2bb1957d1dbe55b59f974f47
Nero Zuo <502350123@qq.com>
2017-03-10 22:19:23 +0800

    Channel implementation

---
+++ CS240h/Lecture2/ChannelImplementation.hs
@@ -0,0 +1,23 @@
+data Item a = Item a (Stream a)
+type Stream a = MVar (Item a)
+data Chan a = Chan (MVar (Stream a)) (MVar (Stream a))
+
+newChan :: IO (Chan a)
+newChan = do
+  empty <- newEmptyMVar
+  liftM2 Chan (newMVar empty) (newMVar empty)
+-- liftM2 is like liftM for functions of two arguments:
+-- liftM2 f m1 m2 = do x1 <- m1; x2 <- m2; return (f x1 x2)
+
+writeChan :: Chan a -> a -> IO ()
+writeChan (Chan _ w) a = do
+  empty <- newEmptyMVar
+  modifyMVar_ w $ \oldEmpty -> do
+    putMVar oldEmpty (Item a empty)
+    return empty
+
+readChan :: Chan a -> IO a
+readChan (Chan r _) =
+    modifyMVar r $ \full -> do
+      (Item a newFull) <- takeMVar full
+      return (newFull, a)
